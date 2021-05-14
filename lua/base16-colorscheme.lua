1eef75abc5d8bb0bf0273b56ad20a3454ccbb27d
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-05-14 09:37:28 -0400

    Change TSComment to be italicized

--- lua/base16-colorscheme.lua
+++ lua/base16-colorscheme.lua
@@ -168,7 +168,7 @@ function M.setup(colors)
     hi.TSAttribute          = { guifg = M.colors.base0A, guibg = nil, gui = NONE,            guisp = nil }
     hi.TSBoolean            = { guifg = M.colors.base09, guibg = nil, gui = NONE,            guisp = nil }
     hi.TSCharacter          = { guifg = M.colors.base0C, guibg = nil, gui = NONE,            guisp = nil }
-    hi.TSComment            = { guifg = M.colors.base03, guibg = nil, gui = NONE,            guisp = nil }
+    hi.TSComment            = { guifg = M.colors.base03, guibg = nil, gui = 'italic',        guisp = nil }
     hi.TSConstructor        = { guifg = M.colors.base0C, guibg = nil, gui = NONE,            guisp = nil }
     hi.TSConditional        = { guifg = M.colors.base0E, guibg = nil, gui = NONE,            guisp = nil }
     hi.TSConstant           = { guifg = M.colors.base09, guibg = nil, gui = NONE,            guisp = nil }
