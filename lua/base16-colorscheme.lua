40c619bbf99ede462bc958a3056c9ecf5d2379dd
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2022-01-15 13:31:50 -0500

    WIP: Add nvim-notify highlight groups

--- lua/base16-colorscheme.lua
+++ lua/base16-colorscheme.lua
@@ -279,6 +279,27 @@ function M.setup(colors)
 
     hi.TreesitterContext = { guifg = nil, guibg = M.colors.base01, gui = 'italic', guisp = nil }
 
+    -- highlight NotifyERRORBorder guifg=#8A1F1F
+    -- highlight NotifyWARNBorder guifg=#79491D
+    -- highlight NotifyINFOBorder guifg=#4F6752
+    -- highlight NotifyDEBUGBorder guifg=#8B8B8B
+    -- highlight NotifyTRACEBorder guifg=#4F3552
+    -- highlight NotifyERRORIcon guifg=#F70067
+    -- highlight NotifyWARNIcon guifg=#F79000
+    -- highlight NotifyINFOIcon guifg=#A9FF68
+    -- highlight NotifyDEBUGIcon guifg=#8B8B8B
+    -- highlight NotifyTRACEIcon guifg=#D484FF
+    -- highlight NotifyERRORTitle  guifg=#F70067
+    -- highlight NotifyWARNTitle guifg=#F79000
+    -- highlight NotifyINFOTitle guifg=#A9FF68
+    -- highlight NotifyDEBUGTitle  guifg=#8B8B8B
+    -- highlight NotifyTRACETitle  guifg=#D484FF
+    -- highlight link NotifyERRORBody Normal
+    -- highlight link NotifyWARNBody Normal
+    -- highlight link NotifyINFOBody Normal
+    -- highlight link NotifyDEBUGBody Normal
+    -- highlight link NotifyTRACEBody Normal
+
     vim.g.terminal_color_0  = M.colors.base00
     vim.g.terminal_color_1  = M.colors.base08
     vim.g.terminal_color_2  = M.colors.base0B
