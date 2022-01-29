ca3ee8343118c53b8bad02d96466be2417f1f855
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2022-01-28 23:34:11 -0500

    Change telescope colours

--- lua/base16-colorscheme.lua
+++ lua/base16-colorscheme.lua
@@ -11,6 +11,50 @@ local M = {
     HINT_ICON    = '',
 }
 
+local HEX_DIGITS = {
+    ['0'] = 0,
+    ['1'] = 1,
+    ['2'] = 2,
+    ['3'] = 3,
+    ['4'] = 4,
+    ['5'] = 5,
+    ['6'] = 6,
+    ['7'] = 7,
+    ['8'] = 8,
+    ['9'] = 9,
+    ['a'] = 10,
+    ['b'] = 11,
+    ['c'] = 12,
+    ['d'] = 13,
+    ['e'] = 14,
+    ['f'] = 15,
+    ['A'] = 10,
+    ['B'] = 11,
+    ['C'] = 12,
+    ['D'] = 13,
+    ['E'] = 14,
+    ['F'] = 15,
+}
+
+local function hex_to_rgb(hex)
+    return HEX_DIGITS[string.sub(hex, 1, 1)] * 16 +  HEX_DIGITS[string.sub(hex, 2, 2)],
+        HEX_DIGITS[string.sub(hex, 3, 3)] * 16 +  HEX_DIGITS[string.sub(hex, 4, 4)],
+        HEX_DIGITS[string.sub(hex, 5, 5)] * 16 +  HEX_DIGITS[string.sub(hex, 6, 6)]
+end
+
+local function rgb_to_hex(r, g, b)
+  return bit.tohex(bit.bor(bit.lshift(r, 16), bit.lshift(g, 8), b), 6)
+end
+
+local function darken(hex, pct)
+    pct = 1 - pct
+    local r, g, b = hex_to_rgb(string.sub(hex, 2))
+    r = math.floor(r * pct)
+    g = math.floor(g * pct)
+    b = math.floor(b * pct)
+    return string.format("#%s", rgb_to_hex(r, g, b))
+end
+
 -- This is a bit of syntactic sugar for creating highlight groups.
 --
 -- local colorscheme = require('colorscheme')
@@ -284,15 +328,18 @@ function M.setup(colors)
 
     hi.TreesitterContext = { guifg = nil, guibg = M.colors.base01, gui = 'italic', guisp = nil }
 
-    hi.TelescopeBorder       = { guifg = M.colors.base01, guibg = M.colors.base01, gui = nil, guisp = nil }
-    hi.TelescopePromptBorder = { guifg = M.colors.base03, guibg = M.colors.base03, gui = nil, guisp = nil }
-    hi.TelescopePromptNormal = { guifg = M.colors.base05, guibg = M.colors.base03, gui = nil, guisp = nil }
-    hi.TelescopePromptPrefix = { guifg = M.colors.base08, guibg = M.colors.base03, gui = nil, guisp = nil }
-    hi.TelescopeNormal       = { guifg = nil,             guibg = M.colors.base01, gui = nil, guisp = nil }
-    hi.TelescopePreviewTitle = { guifg = M.colors.base02, guibg = M.colors.base0B, gui = nil, guisp = nil }
-    hi.TelescopePromptTitle  = { guifg = M.colors.base02, guibg = M.colors.base08, gui = nil, guisp = nil }
-    hi.TelescopeResultsTitle = { guifg = M.colors.base01, guibg = M.colors.base01, gui = nil, guisp = nil }
-    hi.TelescopeSelection    = { guifg = nil,             guibg = M.colors.base03, gui = nil, guisp = nil }
+    local darkerbg = darken(M.colors.base00, 0.1)
+    local darkercursorline = darken(M.colors.base01, 0.1)
+    local darkerstatusline = darken(M.colors.base02, 0.1)
+    hi.TelescopeBorder       = { guifg = darkerbg,         guibg = darkerbg,         gui = nil, guisp = nil }
+    hi.TelescopePromptBorder = { guifg = darkerstatusline, guibg = darkerstatusline, gui = nil, guisp = nil }
+    hi.TelescopePromptNormal = { guifg = M.colors.base05,  guibg = darkerstatusline, gui = nil, guisp = nil }
+    hi.TelescopePromptPrefix = { guifg = M.colors.base08,  guibg = darkerstatusline, gui = nil, guisp = nil }
+    hi.TelescopeNormal       = { guifg = nil,              guibg = darkerbg,         gui = nil, guisp = nil }
+    hi.TelescopePreviewTitle = { guifg = darkercursorline, guibg = M.colors.base0B,  gui = nil, guisp = nil }
+    hi.TelescopePromptTitle  = { guifg = darkercursorline, guibg = M.colors.base08,  gui = nil, guisp = nil }
+    hi.TelescopeResultsTitle = { guifg = darkerbg,         guibg = darkerbg,         gui = nil, guisp = nil }
+    hi.TelescopeSelection    = { guifg = nil,              guibg = darkerstatusline, gui = nil, guisp = nil }
 
     hi.NotifyERRORBorder = { guifg = M.colors.base08, guibg = nil, gui = 'none', guisp = nil }
     hi.NotifyWARNBorder  = { guifg = M.colors.base0E, guibg = nil, gui = 'none', guisp = nil }
