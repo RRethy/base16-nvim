f9ce7474c54803cb0fa308b0b91549d394a07940
RRethy <21000943+RRethy@users.noreply.github.com>
2025-05-26 00:46:45 +0000

    Update with the latest tinted-theming colorschemes

--- README.md
+++ README.md
@@ -140,6 +140,7 @@ base16-eva-dim
 base16-evenok-dark
 base16-everforest
 base16-everforest-dark-hard
+base16-everforest-dark-soft
 base16-flat
 base16-framer
 base16-fruit-soda
---
+++ lua/colors/everforest-dark-soft.lua
@@ -0,0 +1,6 @@
+return {
+    base00 = '#333c43', base01 = '#3a464c', base02 = '#4d5960', base03 = '#859289',
+    base04 = '#9da9a0', base05 = '#d3c6aa', base06 = '#ddd8be', base07 = '#f3ead3',
+    base08 = '#e67e80', base09 = '#e69875', base0A = '#dbbc7f', base0B = '#a7c080',
+    base0C = '#83c092', base0D = '#7fbbb3', base0E = '#d699b6', base0F = '#9da9a0'
+}
