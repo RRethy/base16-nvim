c2f8107967f7b32e6de511f060def3a78c151436
RRethy <21000943+RRethy@users.noreply.github.com>
2026-01-08 00:51:00 +0000

    Update with the latest tinted-theming colorschemes

--- lua/colors/kanagawa-dragon.lua
+++ lua/colors/kanagawa-dragon.lua
@@ -1,6 +1,6 @@
 return {
-    base00 = '#0d0c0c', base01 = '#1d1c19', base02 = '#282727', base03 = '#737c73',
-    base04 = '#a6a69c', base05 = '#c5c9c5', base06 = '#7a8382', base07 = '#c5c9c5',
-    base08 = '#c4746e', base09 = '#b98d7b', base0A = '#c4b28a', base0B = '#87a987',
-    base0C = '#8ea4a2', base0D = '#8ba4b0', base0E = '#8992a7', base0F = '#a292a3'
+    base00 = '#181616', base01 = '#282727', base02 = '#393836', base03 = '#625e5a',
+    base04 = '#737c73', base05 = '#c5c9c5', base06 = '#c8c093', base07 = '#c5c9c5',
+    base08 = '#c4746e', base09 = '#b6927b', base0A = '#c4b28a', base0B = '#8a9a7b',
+    base0C = '#8ea4a2', base0D = '#8ba4b0', base0E = '#a292a3', base0F = '#b98d7b'
 }
